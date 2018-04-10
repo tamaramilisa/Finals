0183d8214287d6fbaed16d4b1e5e5d6d054df782
Tamara Milisa <tamara.milisa@degordian.com>
2018-04-10 13:04:27 +0200

    Initial Commit

---
+++ Finals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Finals
+//
+//  Created by tamara on 10/04/2018.
+//  Copyright © 2018 tamara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

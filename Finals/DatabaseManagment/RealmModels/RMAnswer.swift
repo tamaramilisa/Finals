ae831e3c9b82e6733cb519e96e177c3c001f8b63
Tamara Milisa <tamara.milisa@degordian.com>
2018-06-04 10:52:31 +0200

    Add realm models

---
+++ Finals/DatabaseManagment/RealmModels/RMAnswer.swift
@@ -0,0 +1,9 @@
+//
+//  RMAnswer.swift
+//  Finals
+//
+//  Created by tamara on 04/06/2018.
+//  Copyright © 2018 tamara. All rights reserved.
+//
+
+import Foundation
---
+++ Finals/DatabaseManagment/RealmModels/RMMedal.swift
@@ -0,0 +1,9 @@
+//
+//  RMMedal.swift
+//  Finals
+//
+//  Created by tamara on 04/06/2018.
+//  Copyright © 2018 tamara. All rights reserved.
+//
+
+import Foundation

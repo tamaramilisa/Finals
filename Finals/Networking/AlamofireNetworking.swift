017a5b115cf47bc665c6ba4ab9ffd3eb24ca6b90
tamaramilisa <tamara.milisa@gmail.com>
2018-06-06 23:36:06 +0200

    add createUser

--- Finals/Networking/AlamofireNetworking.swift
+++ Finals/Networking/AlamofireNetworking.swift
@@ -16,4 +16,17 @@ struct AlamofireNetworking {
 	func request(router : AlamofireRouter) -> Observable<(HTTPURLResponse, Any)> {
 		return RxAlamofire.requestJSON(router.method, router.url, parameters: router.params, encoding: router.encoding, headers: router.headers).observeOn(MainScheduler.instance)
 	}
+    
+    func createUser(firstName: String, lastName: String, email:String, password: String) -> Observable<(HTTPURLResponse, Any)> {
+        
+        let params: [String: Any]? = [
+            "first_name" : firstName,
+            "last_name" : lastName,
+            "email" : email,
+            "password" : password,
+            "confirm_password" : password
+        ]
+        
+        return request(router: FinalsRouter.PostNoAuth(path: "/signup", params: params as [String : AnyObject]?)).debug()
+    }
 }

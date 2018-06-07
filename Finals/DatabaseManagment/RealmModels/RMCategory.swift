f2963c6ba0ac49a0b4b8733795c94b4ccfe7fd39
Tamara Milisa <tamara.milisa@degordian.com>
2018-06-07 10:39:11 +0200

    add RMcategory

--- Finals/DatabaseManagment/RealmModels/RMCategory.swift
+++ Finals/DatabaseManagment/RealmModels/RMCategory.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import RealmSwift
+import ObjectMapper
+import ObjectMapper_Realm
+
+
+class RMCategory: Object, Mappable {
+    
+    @objc dynamic var id: Int = 0
+    @objc dynamic var name: String?
+//    @objc dynamic var email: String?
+    
+    
+    override static func primaryKey() -> String {
+        return "id"
+    }
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        id <- (map["id"], TransformOf<Int, String>(fromJSON: { Int($0!) }, toJSON: { $0.map { String($0) } }))
+        name <- map["name"]
+//        email <- map["email"]
+    }
+    
+}
